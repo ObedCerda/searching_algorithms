21ec97e4e3c6692b3c840e2e370b694e3e0c94d5
Obed Cerda <obed.cerda.dev@gmail.com>
2026-01-09 17:52:45 -0600

    linear search with an excluded term

---
+++ linear_search.py
@@ -0,0 +1,24 @@
+
+# we set the random array between 0 and 50 and 50 size (being consistent with choices)
+import numpy as np
+
+def linear_search(array, target):
+    for idx, value in enumerate(array):
+        if value == target:
+            return idx
+    return None
+
+if __name__ == "__main__":
+    np.random.seed(42)
+    bucket = np.arange(0,51)
+    arr = np.random.choice(bucket, size=50, replace=False)
+
+    for idx, value in enumerate(arr):
+        print(f"Index: {idx}, Value: {value}")
+    target = 38 #the only one excluded from the bucket
+    response = linear_search(arr, target)
+    print("="*30)
+    if response is None:
+        print("Value not found in the array")
+    else:
+        print(f"Value found un index {response}")
